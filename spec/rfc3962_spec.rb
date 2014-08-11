04688e4f66651482a288ad1d2e735871b1227c37
Robin Tweedie <robin@songkick.com>
2014-08-11 12:51:47 +0100

    Convert specs to RSpec 3.0.3 syntax with Transpec
    This conversion is done by Transpec 2.3.6 with the following command:
        transpec -f
    
    * 29 conversions
        from: == expected
          to: eq(expected)
    
    * 24 conversions
        from: obj.should
          to: expect(obj).to
    
    * 7 conversions
        from: lambda { }.should
          to: expect { }.to
    
    * 5 conversions
        from: obj.should_not
          to: expect(obj).not_to
    
    * 1 conversion
        from: lambda { }.should_not
          to: expect { }.not_to
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/rfc3962_spec.rb
+++ spec/rfc3962_spec.rb
@@ -21,13 +21,13 @@ describe PBKDF2, "when deriving keys" do
     end
     
     expected = "cd ed b5 28 1b b2 f8 01 56 5a 11 22 b2 56 35 15"
-    p.hex_string.should == expected.gsub(' ','')
+    expect(p.hex_string).to eq(expected.gsub(' ',''))
     
     expected =  "cd ed b5 28 1b b2 f8 01 56 5a 11 22 b2 56 35 15" +
                 "0a d1 f7 a0 4b b9 f3 a3 33 ec c0 e2 e1 f7 08 37"
     
     p.key_length = 256/8
-    p.hex_string.should == expected.gsub(' ','')
+    expect(p.hex_string).to eq(expected.gsub(' ',''))
   end
 
   it "should match the second test case in appendix B of RFC 3962" do
@@ -48,12 +48,12 @@ describe PBKDF2, "when deriving keys" do
     end
     
     expected = "01 db ee 7f 4a 9e 24 3e 98 8b 62 c7 3c da 93 5d"
-    p.hex_string.should == expected.gsub(' ','')
+    expect(p.hex_string).to eq(expected.gsub(' ',''))
     
     expected =  "01 db ee 7f 4a 9e 24 3e 98 8b 62 c7 3c da 93 5d" + 
                 "a0 53 78 b9 32 44 ec 8f 48 a9 9e 61 ad 79 9d 86"
     p.key_length = 256/8
-    p.hex_string.should == expected.gsub(' ','')
+    expect(p.hex_string).to eq(expected.gsub(' ',''))
   end
 
   it "should match the third test case in appendix B of RFC 3962" do
@@ -74,12 +74,12 @@ describe PBKDF2, "when deriving keys" do
     end
     
     expected = "5c 08 eb 61 fd f7 1e 4e 4e c3 cf 6b a1 f5 51 2b"
-    p.hex_string.should == expected.gsub(' ','')
+    expect(p.hex_string).to eq(expected.gsub(' ',''))
     
     expected =  "5c 08 eb 61 fd f7 1e 4e 4e c3 cf 6b a1 f5 51 2b" +
                 "a7 e5 2d db c5 e5 14 2f 70 8a 31 e2 e6 2b 1e 13"
     p.key_length = 256/8
-    p.hex_string.should == expected.gsub(' ','')
+    expect(p.hex_string).to eq(expected.gsub(' ',''))
   end
 
   it "should match the fourth test case in appendix B of RFC 3962" do
@@ -100,12 +100,12 @@ describe PBKDF2, "when deriving keys" do
     end
     
     expected = "d1 da a7 86 15 f2 87 e6 a1 c8 b1 20 d7 06 2a 49"
-    p.hex_string.should == expected.gsub(' ','')
+    expect(p.hex_string).to eq(expected.gsub(' ',''))
     
     expected =  "d1 da a7 86 15 f2 87 e6 a1 c8 b1 20 d7 06 2a 49" +
                 "3f 98 d2 03 e6 be 49 a6 ad f4 fa 57 4b 6e 64 ee"
     p.key_length = 256/8
-    p.hex_string.should == expected.gsub(' ','')
+    expect(p.hex_string).to eq(expected.gsub(' ',''))
   end
 
   it "should match the fifth test case in appendix B of RFC 3962" do
@@ -127,12 +127,12 @@ describe PBKDF2, "when deriving keys" do
     end
     
     expected = "13 9c 30 c0 96 6b c3 2b a5 5f db f2 12 53 0a c9"
-    p.hex_string.should == expected.gsub(' ','')
+    expect(p.hex_string).to eq(expected.gsub(' ',''))
     
     expected =  "13 9c 30 c0 96 6b c3 2b a5 5f db f2 12 53 0a c9" +
                 "c5 ec 59 f1 a4 52 f5 cc 9a d9 40 fe a0 59 8e d1"
     p.key_length = 256/8
-    p.hex_string.should == expected.gsub(' ','')
+    expect(p.hex_string).to eq(expected.gsub(' ',''))
   end
   
   it "should match the sixth test case in appendix B of RFC 3962" do
@@ -154,12 +154,12 @@ describe PBKDF2, "when deriving keys" do
     end
     
     expected = "9c ca d6 d4 68 77 0c d5 1b 10 e6 a6 87 21 be 61"
-    p.hex_string.should == expected.gsub(' ','')
+    expect(p.hex_string).to eq(expected.gsub(' ',''))
     
     expected =  "9c ca d6 d4 68 77 0c d5 1b 10 e6 a6 87 21 be 61" +
                 "1a 8b 4d 28 26 01 db 3b 36 be 92 46 91 5e c8 2a"
     p.key_length = 256/8
-    p.hex_string.should == expected.gsub(' ','')
+    expect(p.hex_string).to eq(expected.gsub(' ',''))
   end  
   
   it "should match the seventh test case in appendix B of RFC 3962" do
@@ -185,11 +185,11 @@ describe PBKDF2, "when deriving keys" do
     end
     
     expected = "6b 9c f2 6d 45 45 5a 43 a5 b8 bb 27 6a 40 3b 39"
-    p.hex_string.should == expected.gsub(' ','')
+    expect(p.hex_string).to eq(expected.gsub(' ',''))
     
     expected =  "6b 9c f2 6d 45 45 5a 43 a5 b8 bb 27 6a 40 3b 39" +
                 "e7 fe 37 a0 c4 1e 02 c2 81 ff 30 69 e1 e9 4f 52"
     p.key_length = 256/8
-    p.hex_string.should == expected.gsub(' ','')
+    expect(p.hex_string).to eq(expected.gsub(' ',''))
   end
 end
